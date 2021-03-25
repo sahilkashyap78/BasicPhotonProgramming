e694e18bd6bc1350c28ce9df2421da1036247645
sahilkashyap78 <sahilkashyap78@gmail.com>
2021-03-26 01:14:04 +0530

    had learned some photon basics

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    public static NetworkManager instance;
+
+    void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        // first we connect to the master server
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to the master server");
+        CreateRoom("TestRoom");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("RoomCreated "+" "+PhotonNetwork.CurrentRoom.Name);
+    }
+    public void CreateRoom(string roomName)
+    {
+        // for creating the room and passed the room name
+        PhotonNetwork.CreateRoom(roomName);
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        // for joining the room
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+}
